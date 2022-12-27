1ffa28758081adee843042ba64d8e84f0d2b3bc8
driftyy777 <dhimandeepak517@gmail.com>
2022-12-27 16:34:23 +0530

    minor problems removed

--- lib/main.dart
+++ lib/main.dart
@@ -10,85 +10,123 @@ void main() {
   runApp(Abc());
 }
 
-void eatItSnackBar(String message) {
-  SnackBar snackBar = SnackBar(
-    backgroundColor: const Color.fromARGB(213, 0, 0, 0),
+eatItSnackBar(context, String message) {
+  var snackBar = SnackBar(
     clipBehavior: Clip.antiAliasWithSaveLayer,
     dismissDirection: DismissDirection.startToEnd,
+    backgroundColor: const Color.fromARGB(213, 5, 5, 5),
     behavior: SnackBarBehavior.floating,
     shape: const RoundedRectangleBorder(
       borderRadius: BorderRadius.all(
         Radius.circular(5),
       ),
     ),
-    content: Text(message),
+    content: Text(
+      message,
+      style: const TextStyle(
+        color: Color.fromARGB(255, 255, 255, 255),
+        fontSize: 16,
+        fontFamily: "Roboto",
+        fontWeight: FontWeight.w400,
+      ),
+    ),
   );
-  snackbarKey.currentState?.showSnackBar(snackBar);
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
 }
 
 class Abc extends StatelessWidget {
-  snackeatit(BuildContext context, String msg) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      content: Text(msg),
-    ));
-  }
-
-  still() async {
+  still(context) async {
     var file_paths;
+    var saving_path;
+
     var status = await Permission.storage.status;
     if (!status.isGranted) {
       await Permission.storage.request();
     }
-
     var whatsAppBusuiness =
         '/storage/emulated/0/Android/media/com.whatsapp.w4b/WhatsApp Business/Media/.Statuses';
     var whatsApp =
         '/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses';
-
     if (Directory(whatsAppBusuiness).existsSync()) {
       file_paths = whatsAppBusuiness;
-      print("WhatsApp Business");
     } else if (Directory(whatsApp).existsSync()) {
       file_paths = whatsApp;
-      print("WhatsApp");
     } else {
-      print("No WhatsApp folder found");
-      eatItSnackBar("Path not found");
+      eatItSnackBar(context, "your phone sucks");
     }
 
-    var saved = '/storage/emulated/0/Dcim/Camera';
+    var camera = '/storage/emulated/0/DCIM/Camera';
+    var screenShot = '/storage/emulated/0/DCIM/Screenshots';
+    var pictures = '/storage/emulated/0/Pictures';
+    var download = '/storage/emulated/0/Download';
+
+    if (Directory(camera).existsSync()) {
+      saving_path = camera;
+    } else if (Directory(screenShot).existsSync()) {
+      saving_path = screenShot;
+    } else if (Directory(pictures).existsSync()) {
+      saving_path = pictures;
+    } else if (Directory(download).existsSync()) {
+      saving_path = download;
+    } else if (Directory(download).existsSync()) {
+      saving_path = download;
+    } else {
+      saving_path = '/storage/emulated/0';
+    }
+
     var directory = Directory(file_paths);
     var files = directory.listSync();
     var count = 0;
     for (var e in files) {
       var name = e.path.split('/').last;
-      print(name);
       if (name == ".nomedia") continue;
-      var newLocation = '${saved}/${count.toString()}.${name.split(".").last}';
-      print("f  " + newLocation);
+      // var newLocation = '${saved}/${count.toString()}.${name.split(".").last}';
+      var newLocation = '$saving_path/${count.toString()}.$name';
       var file = File(e.path);
       file.copySync(newLocation);
       MediaScanner.loadMedia(path: newLocation.toString());
-      print("Copied");
-      eatItSnackBar("Done");
       count++;
     }
+    eatItSnackBar(context, "Done");
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 0, 0, 0),
+          foregroundColor: Color.fromARGB(255, 255, 255, 255),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+            foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+          ),
+        ),
+      ),
       home: Scaffold(
-        body: Builder(builder: (context) {
-          return Center(
-            child: ElevatedButton(
-              child: const Text("Button"),
-              onPressed: () {
-                still();
-              },
-            ),
-          );
-        }),
+        body: Builder(
+          builder: (context) {
+            return Scaffold(
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      child: const Text("Click"),
+                      onPressed: () {
+                        still(context);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
