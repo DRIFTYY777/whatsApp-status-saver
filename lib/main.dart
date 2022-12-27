e0a09b48cb06ddda582682624c43f29ddf1214b9
driftyy777 <dhimandeepak517@gmail.com>
2022-12-27 15:38:16 +0530

    checking whatsApp or whatsApp business

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,27 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:media_scanner/media_scanner.dart';
 
+final GlobalKey<ScaffoldMessengerState> snackbarKey =
+    GlobalKey<ScaffoldMessengerState>();
+
 void main() {
   runApp(Abc());
+}
+
+void eatItSnackBar(String message) {
+  SnackBar snackBar = SnackBar(
+    backgroundColor: const Color.fromARGB(213, 0, 0, 0),
+    clipBehavior: Clip.antiAliasWithSaveLayer,
+    dismissDirection: DismissDirection.startToEnd,
+    behavior: SnackBarBehavior.floating,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(5),
+      ),
+    ),
+    content: Text(message),
+  );
+  snackbarKey.currentState?.showSnackBar(snackBar);
 }
 
 class Abc extends StatelessWidget {
@@ -15,12 +34,28 @@ class Abc extends StatelessWidget {
   }
 
   still() async {
+    var file_paths;
     var status = await Permission.storage.status;
     if (!status.isGranted) {
       await Permission.storage.request();
     }
-    var file_paths =
+
+    var whatsAppBusuiness =
         '/storage/emulated/0/Android/media/com.whatsapp.w4b/WhatsApp Business/Media/.Statuses';
+    var whatsApp =
+        '/storage/emulated/0/Android/media/com.whatsapp/WhatsApp/Media/.Statuses';
+
+    if (Directory(whatsAppBusuiness).existsSync()) {
+      file_paths = whatsAppBusuiness;
+      print("WhatsApp Business");
+    } else if (Directory(whatsApp).existsSync()) {
+      file_paths = whatsApp;
+      print("WhatsApp");
+    } else {
+      print("No WhatsApp folder found");
+      eatItSnackBar("Path not found");
+    }
+
     var saved = '/storage/emulated/0/Dcim/Camera';
     var directory = Directory(file_paths);
     var files = directory.listSync();
@@ -35,6 +70,7 @@ class Abc extends StatelessWidget {
       file.copySync(newLocation);
       MediaScanner.loadMedia(path: newLocation.toString());
       print("Copied");
+      eatItSnackBar("Done");
       count++;
     }
   }
